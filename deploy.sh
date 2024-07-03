7580ca53990291a41288c0422d8e1e15a801312a
David Majdandžić <david.majdandzic@hotmail.com>
2024-07-03 13:30:59 +0200

    Enable build for linux

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-go build main && cp main.exe "/c/Program Files/Git/usr/bin/cln.exe"
+GOOS=windows GOARCH=amd64 go build -o main.exe main && cp main.exe "/c/Program Files/Git/usr/bin/cln.exe"
+GOOS=linux GOARCH=amd64 go build -o main_linux main
